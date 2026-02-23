4dd61749c24938107f01ca80ef740ba31f803f6b
FadiMagdi <fadimagdi97@yahoo.com>
2026-02-23 09:49:25 -0500

    added product grid

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,47 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
+
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+ let total = 0;
+  cart.forEach(item => {
+    const { quantity, cost } = item;
+    const itemTotal = parseFloat(cost.substring(1)) * quantity;
+    total += itemTotal;
+  });
+  return total;
+
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+        dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1))* item.quantity;
   };
 
   return (
